e8db007ff9b59addfe672c12966888e99ceabd4f
Jin-Byun <bjsubway12@gmail.com>
2025-05-20 17:05:08 -0700

    chore: update for removal of injected

--- manifest.ts
+++ manifest.ts
@@ -4,46 +4,45 @@ import packageJson from "./package.json";
  * After changing, please reload the extension at `chrome://extensions`
  */
 const manifest: chrome.runtime.ManifestV3 = {
-  manifest_version: 3,
-  name: packageJson.name,
-  version: packageJson.version,
-  description: packageJson.description,
-  permissions: ["activeTab", "tabs", "webRequest", "storage"], // storage later for storage.sync
-  host_permissions: ["https://*/*"],
-  externally_connectable: {
-    matches: ["https://www.youtube.com/*"],
-  },
-  background: {
-    service_worker: "src/background/index.js",
-    type: "module",
-  },
-  action: {
-    default_title: "My YT Organizer",
-    default_icon: "icon-34.png",
-  },
-  icons: {
-    "128": "icon-128.png",
-  },
-  content_scripts: [
-    {
-      matches: ["https://*.youtube.com/*"],
-      js: ["src/content/index.js"],
-      css: ["assets/css/Styles<KEY>.chunk.css"],
-    },
-  ],
-  web_accessible_resources: [
-    {
-      resources: [
-        "assets/js/*.js",
-        "src/injected/index.js",
-        "assets/css/*.css",
-        "icon-128.png",
-        "icon-34.png",
-        "Icon_folder_2019_1.svg",
-      ],
-      matches: ["*://*/*"],
-    },
-  ],
+	manifest_version: 3,
+	name: packageJson.name,
+	version: packageJson.version,
+	description: packageJson.description,
+	permissions: ["activeTab", "tabs", "webRequest", "storage"], // storage later for storage.sync
+	host_permissions: ["https://*/*"],
+	externally_connectable: {
+		matches: ["https://www.youtube.com/*"],
+	},
+	background: {
+		service_worker: "src/background/index.js",
+		type: "module",
+	},
+	action: {
+		default_title: "My YT Organizer",
+		default_icon: "icon-34.png",
+	},
+	icons: {
+		"128": "icon-128.png",
+	},
+	content_scripts: [
+		{
+			matches: ["https://*.youtube.com/*"],
+			js: ["src/content/index.js"],
+			css: ["assets/css/Styles<KEY>.chunk.css"],
+		},
+	],
+	web_accessible_resources: [
+		{
+			resources: [
+				"assets/js/*.js",
+				"assets/css/*.css",
+				"icon-128.png",
+				"icon-34.png",
+				"Icon_folder_2019_1.svg",
+			],
+			matches: ["*://*/*"],
+		},
+	],
 };
 
 export default manifest;
--- vite.config.ts
+++ vite.config.ts
@@ -15,55 +15,54 @@ const isProduction = !isDev;
 const enableHmrInBackgroundScript = true;
 
 export default defineConfig({
-  plugins: [
-    makeManifest(manifest, publicDir, outDir, {
-      isDev,
-      contentScriptCssKey: regenerateCacheInvalidationKey(),
-    }),
-    watchRebuild(),
-  ],
-  publicDir,
-  build: {
-    outDir,
-    minify: isProduction,
-    reportCompressedSize: isProduction,
-    rollupOptions: {
-      input: {
-        background: resolve(srcDir, "background.ts"),
-        content: resolve(srcDir, "contentScript.ts"),
-        injected: resolve(srcDir, "injected.ts"),
-        contentStyle: resolve(srcDir, "styles.css"),
-      },
-      output: {
-        entryFileNames: "src/[name]/index.js",
-        chunkFileNames: isDev
-          ? "assets/js/[name].js"
-          : "assets/js/[name].[hash].js",
-        assetFileNames: (assetInfo) => {
-          const { dir, name: _name } = path.parse(assetInfo.name);
-          const assetFolder = dir.split("/").at(-1);
-          const name = assetFolder + firstUpperCase(_name);
-          if (name === "Contentstyle") {
-            return `assets/css/Styles${cacheInvalidationKey}.chunk.css`;
-          }
-          return `assets/[ext]/${name}.chunk.[ext]`;
-        },
-      },
-    },
-  },
+	plugins: [
+		makeManifest(manifest, publicDir, outDir, {
+			isDev,
+			contentScriptCssKey: regenerateCacheInvalidationKey(),
+		}),
+		watchRebuild(),
+	],
+	publicDir,
+	build: {
+		outDir,
+		minify: isProduction,
+		reportCompressedSize: isProduction,
+		rollupOptions: {
+			input: {
+				background: resolve(srcDir, "background.ts"),
+				content: resolve(srcDir, "contentScript.ts"),
+				contentStyle: resolve(srcDir, "styles.css"),
+			},
+			output: {
+				entryFileNames: "src/[name]/index.js",
+				chunkFileNames: isDev
+					? "assets/js/[name].js"
+					: "assets/js/[name].[hash].js",
+				assetFileNames: (assetInfo) => {
+					const { dir, name: _name } = path.parse(assetInfo.name);
+					const assetFolder = dir.split("/").at(-1);
+					const name = assetFolder + firstUpperCase(_name);
+					if (name === "Contentstyle") {
+						return `assets/css/Styles${cacheInvalidationKey}.chunk.css`;
+					}
+					return `assets/[ext]/${name}.chunk.[ext]`;
+				},
+			},
+		},
+	},
 });
 
 function firstUpperCase(str: string) {
-  const firstAlphabet = new RegExp(/( |^)[a-z]/, "g");
-  return str.toLowerCase().replace(firstAlphabet, (L) => L.toUpperCase());
+	const firstAlphabet = new RegExp(/( |^)[a-z]/, "g");
+	return str.toLowerCase().replace(firstAlphabet, (L) => L.toUpperCase());
 }
 
 let cacheInvalidationKey: string = generateKey();
 function regenerateCacheInvalidationKey() {
-  cacheInvalidationKey = generateKey();
-  return cacheInvalidationKey;
+	cacheInvalidationKey = generateKey();
+	return cacheInvalidationKey;
 }
 
 function generateKey(): string {
-  return `${(Date.now() / 100).toFixed()}`;
+	return `${(Date.now() / 100).toFixed()}`;
 }
